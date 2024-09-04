6f0a32317bc547d9f6e278b99f88c19536e9ddbb
HoangTuan <tuanhoang2801@gmail.com>
2024-09-04 22:30:09 +0700

    First Commit

---
+++ src/database/Database.java
@@ -0,0 +1,46 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+public class Database {
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+			String url = "jdbc:sqlserver://localhost:1433;databasename=NhaSach;encrypt=true;trustServerCertificate=true";
+			String username = "sa";
+			String password = "hoanganhtuan";
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+	
+	public static void closeConnection(Connection conn) {
+		try {
+			if(conn!=null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void printInfo(Connection conn) {
+		if(conn!=null) {
+			try {
+				System.out.println(conn.getMetaData().toString());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
